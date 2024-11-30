ed2c0b6f407730f5eb80c6eb28130b69b60a00c0
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-30 10:56:16 -0600

    updated mentor

--- mentor/admin.py
+++ mentor/admin.py
@@ -3,12 +3,12 @@ from .models import Mentor
 
 
 class MentorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'designation', 'email', 'phone', 'expertise')  # Fields to display in the list view
+    list_display = ('name', 'designation', 'email', 'phone', 'expertise','linkedIn_id')  # Fields to display in the list view
     search_fields = ('name', 'email', 'designation')  # Fields that can be searched
     list_filter = ('designation', 'expertise')  # Filters to use on the list view
 
     # Optional: You can also customize the form to be displayed when adding or editing a mentor
-    fields = ('name', 'designation', 'expertise', 'email', 'phone', 'photo', 'bio')  # Form fields to display
-    readonly_fields = ('email',)  # Make the email field read-only, if desired
+    fields = ('name', 'designation', 'expertise', 'email', 'phone', 'photo', 'bio', 'linkedIn_id')  # Form fields to display
+    
 
 admin.site.register(Mentor,MentorAdmin)
